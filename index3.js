261870034226f84854d20fbe314a71127d1bea0d
Devendra8871 <167069533+Devendra8871@users.noreply.github.com>
2026-03-05 22:34:08 +0530

    Initial commit

---
+++ index3.js
@@ -0,0 +1,38 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "yellow", "purple", "orange"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function () {
+      if (started == false) {
+            console.log("Game is started");
+            started = true;
+
+            levelup();
+      }
+});
+
+function btnFlash(btn) {
+      btn.classList.add("flash");
+      setTimeout(function () {
+            btn.classList.remove("flesh");
+      }, 250);
+}
+
+function levelup() {
+      level++;
+      h2.innerText = `Level ${level}`;
+
+      let randIdx = Math.floor(Math.random() * 3);
+      let randColor = btns[randIdx];
+      let randBtn = document.querySelector(`.${randColor}`);
+      console.log(randIdx);
+      console.log(randColor);
+      console.log(randBtn);
+      btnFlash(randBtn);
+}
